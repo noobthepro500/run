5857f42aebefb0b38ecc2dc388e6caa4ea565da9
noobthepro500 <65024524+noobthepro500@users.noreply.github.com>
2023-05-08 21:14:05 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,8 +1,14 @@
-$uploaddir = '/images/';
-$uploadfile = $uploaddir . basename($_FILES['my_files']['name']);
-
-if (move_uploaded_file($_FILES['my_files']['my_name'], $uploadfile)) {
-echo "success";
-} else {
-echo "error";
-}
+<?php
+   $destination_path = getcwd().DIRECTORY_SEPARATOR;
+ 
+   $result = 0;
+ 
+   $target_path = $destination_path . basename( $_FILES['myfile']['name']);
+ 
+   if(@move_uploaded_file($_FILES['myfile']['tmp_name'], $target_path)) {
+      $result = 1;
+   }
+ 
+   sleep(1);
+?>
+ 
